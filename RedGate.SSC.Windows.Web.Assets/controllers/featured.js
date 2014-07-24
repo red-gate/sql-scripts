d4feee9f3b11d8cbe8ab9c895c51d2d78977cd47
Kevin Boyle <kevin.boyle@outlook.com>
2014-07-24 14:27:15 +0100

    don't send auth to any external service

--- RedGate.SSC.Windows.Web.Assets/controllers/featured.js
+++ RedGate.SSC.Windows.Web.Assets/controllers/featured.js
@@ -3,6 +3,11 @@ FeaturedController.$inject = ['$scope', '$http', '$log', 'scripts'];
 
 function FeaturedController($scope, $http, $log, scripts) {
     $scope.loading.isLoading = true;
+
+    //This is a little bit of a hack. We are about to send a request to an external service, make sure we don't send auth
+    var auth = $http.defaults.headers.common.Authorization;
+    $http.defaults.headers.common = {};
+
     $http.get('http://www.red-gate.com/products/sql-server-central/plugin/featured').success(function (data) {
         $scope.loading.isLoading = false;
         $scope.loading.pageLoaded = true;
@@ -12,6 +17,8 @@ function FeaturedController($scope, $http, $log, scripts) {
         $log.error('unable to get featured scripts');
         $scope.loading.isLoading = false;
         $scope.loading.pageLoaded = true;
+    }).finally(function() {
+        $http.defaults.headers.common.Authorization = auth; //Re-add auth for subsequent visits to SSC
     });
 
     $scope.open = function (scriptId) {
